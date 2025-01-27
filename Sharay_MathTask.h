d4efefc27f08cf865a4865861628ff4954f15dec
Natalia <nsharay31@mail.ru>
2025-01-27 17:36:20 +0300

    Add Sharay_MathTask.h

---
+++ Sharay_MathTask.h
@@ -0,0 +1,24 @@
+#include <string>
+
+using namespace std;
+
+bool UserInput(string input) { 
+    if (input.empty()) return false;
+    try {
+        int number = stod(input);
+        if (number < 0)
+        return false;
+｝
+catch(...)
+{return false;}
+return true;}
+
+double CalcArith(double num_x, double num_y)
+{
+    return (num_x+ num_y)/2;
+}
+
+double CalcGeom(double num_x, double num_y)
+{
+    return sqrt(num_x * num_y);
+}
